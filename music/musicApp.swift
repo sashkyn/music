c192cdbb5873558b83670f3c2c54a589b3b1851f
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-06-25 16:00:00 +0200

    Initial Commit

---
+++ music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by Aleksandr Martseniuk on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
